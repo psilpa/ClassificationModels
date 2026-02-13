dd5261633f949f497bcca0af23081f7a771912d7
psilpa <pvsilpa@gmail.com>
2026-02-13 19:32:44 +0530

    Add files via upload

---
+++ model/saved_models/xgboost_model.py
@@ -0,0 +1,14 @@
+from xgboost import XGBClassifier
+from train_utils import load_and_split_data, evaluate_model
+
+X_train, X_test, y_train, y_test = load_and_split_data("data/heart.csv")
+
+model = XGBClassifier(
+    use_label_encoder=False,
+    eval_metric="logloss",
+    random_state=42
+)
+model.fit(X_train, y_train)
+
+metrics = evaluate_model(model, X_test, y_test)
+print("XGBoost:", metrics)
